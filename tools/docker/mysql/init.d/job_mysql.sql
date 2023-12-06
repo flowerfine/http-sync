9820026b0b656a8e42ed125540474bd39ff70341
wangqi <1942460489@qq.com>
2023-12-06 11:33:34 +0800

    feature: 任务抽象

---
+++ tools/docker/mysql/init.d/job_mysql.sql
@@ -0,0 +1,61 @@
+create database if not exists data_service default character set utf8mb4 collate utf8mb4_unicode_ci;
+use data_service;
+
+CREATE TABLE `job_group`
+(
+    `id`           bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
+    `group`        varchar(64)         NOT NULL COMMENT '分组',
+    `creator`      varchar(32)         NOT NULL DEFAULT 'system' COMMENT '创建人',
+    `modifier`     varchar(32)         NOT NULL DEFAULT 'system' COMMENT '修改人',
+    `is_deleted`   tinyint(4)          NOT NULL DEFAULT '0' COMMENT '删除标识。0: 未删除, 1: 已删除',
+    `gmt_create`   datetime            NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `gmt_modified` datetime            NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `uniq_group` (`group`)
+) ENGINE = InnoDB COMMENT ='任务分组';
+
+CREATE TABLE `job_info`
+(
+    `id`           bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
+    `group_id`     bigint(20)          NOT NULL COMMENT '分组id',
+    `job`          varchar(255)        NOT NULL COMMENT '任务',
+    `sub_job`      varchar(255)        NOT NULL COMMENT '子任务',
+    `creator`      varchar(32)         NOT NULL DEFAULT 'system' COMMENT '创建人',
+    `modifier`     varchar(32)         NOT NULL DEFAULT 'system' COMMENT '修改人',
+    `is_deleted`   tinyint(4)          NOT NULL DEFAULT '0' COMMENT '删除标识。0: 未删除, 1: 已删除',
+    `gmt_create`   datetime            NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `gmt_modified` datetime            NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `uniq_job` (`group_id`, `job`, `sub_job`)
+) ENGINE = InnoDB COMMENT ='任务信息';
+
+CREATE TABLE `job_authorization`
+(
+    `id`            bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
+    `account`       varchar(255)        NOT NULL COMMENT '主账号',
+    `sub_account`   varchar(255)        NOT NULL COMMENT '子账号',
+    `authorization` varchar(255) COMMENT '授权信息',
+    `creator`       varchar(32)         NOT NULL DEFAULT 'system' COMMENT '创建人',
+    `modifier`      varchar(32)         NOT NULL DEFAULT 'system' COMMENT '修改人',
+    `is_deleted`    tinyint(4)          NOT NULL DEFAULT '0' COMMENT '删除标识。0: 未删除, 1: 已删除',
+    `gmt_create`    datetime            NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `gmt_modified`  datetime            NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `uniq_account` (`account`, `sub_account`)
+) ENGINE = InnoDB COMMENT ='任务授权';
+
+CREATE TABLE `job_sync_offset`
+(
+    `id`               bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
+    `job_id`           bigint(20)          NOT NULL COMMENT '任务id',
+    `authorization_id` bigint(20)          NOT NULL COMMENT '授权id',
+    `last_sync_offset` varchar(255) COMMENT '上次进度位点',
+    `sync_offset`      varchar(255) COMMENT '进度位点',
+    `creator`          varchar(32)         NOT NULL DEFAULT 'system' COMMENT '创建人',
+    `modifier`         varchar(32)         NOT NULL DEFAULT 'system' COMMENT '修改人',
+    `is_deleted`       tinyint(4)          NOT NULL DEFAULT '0' COMMENT '删除标识。0: 未删除, 1: 已删除',
+    `gmt_create`       datetime            NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `gmt_modified`     datetime            NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `uniq_key` (`job_id`, `authorization_id`)
+) ENGINE = InnoDB COMMENT ='任务进度位点';
--- tools/docker/mysql/init.d/jst_order.sql
+++ tools/docker/mysql/init.d/jst_order.sql
@@ -1,21 +1,6 @@
 create database if not exists data_service default character set utf8mb4 collate utf8mb4_unicode_ci;
 use data_service;
 
-CREATE TABLE `job_sync_offset`
-(
-    `id`           bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
-    `method`       varchar(64)         NOT NULL COMMENT '接口名称',
-    `start_time`   datetime            NOT NULL COMMENT '修改起始时间',
-    `end_time`     datetime            NOT NULL COMMENT '修改结束时间',
-    `creator`      varchar(32)         NOT NULL DEFAULT 'system' COMMENT '创建人',
-    `modifier`     varchar(32)         NOT NULL DEFAULT 'system' COMMENT '修改人',
-    `is_deleted`   tinyint(4)          NOT NULL DEFAULT '0' COMMENT '删除标识。0: 未删除, 1: 已删除',
-    `gmt_create`   datetime            NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `gmt_modified` datetime            NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `uniq_method` (`method`)
-) ENGINE = InnoDB COMMENT ='同步进度位点';
-
 CREATE TABLE `jst_order`
 (
     `id`                 bigint(20)  NOT NULL AUTO_INCREMENT COMMENT '主键ID',
