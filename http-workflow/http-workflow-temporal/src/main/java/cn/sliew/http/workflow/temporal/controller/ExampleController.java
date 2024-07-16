cfa69e4c308747722368ee938ae760719daab85d
wangqi <wangqi@xinxuan.net>
2024-07-16 15:36:06 +0800

    feature: update README

--- http-workflow/http-workflow-temporal/src/main/java/cn/sliew/http/workflow/temporal/controller/ExampleController.java
+++ http-workflow/http-workflow-temporal/src/main/java/cn/sliew/http/workflow/temporal/controller/ExampleController.java
@@ -2,8 +2,11 @@ package cn.sliew.http.workflow.temporal.controller;
 
 import cn.sliew.http.workflow.temporal.workflow.hello.HelloWorkflow;
 import cn.sliew.http.workflow.temporal.workflow.hello.Person;
+import io.temporal.api.common.v1.WorkflowExecution;
 import io.temporal.client.WorkflowClient;
 import io.temporal.client.WorkflowOptions;
+import io.temporal.client.WorkflowStub;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/temporal/example")
 public class ExampleController {
@@ -30,6 +34,23 @@ public class ExampleController {
 
         // bypass thymeleaf, don't return template name just result
         return new ResponseEntity<>("\"" + workflow.sayHello(person) + "\"", HttpStatus.OK);
+    }
+
+    @GetMapping("/untyped-hello")
+    public ResponseEntity<String> untypedHelloSample(Person person) {
+        WorkflowStub workflowStub =
+                client.newUntypedWorkflowStub(
+                        HelloWorkflow.class.getSimpleName(),
+                        WorkflowOptions.newBuilder()
+                                .setTaskQueue("HelloSampleTaskQueue")
+                                .setWorkflowId("UntypedHelloSample")
+                                .build());
+
+        WorkflowExecution execution = workflowStub.start(person);
+        log.info("工作流启动! workflowId: {}, runId: {}", execution.getWorkflowId(), execution.getRunId());
+
+        // bypass thymeleaf, don't return template name just result
+        return new ResponseEntity<>("\"" + workflowStub.getResult(String.class) + "\"", HttpStatus.OK);
     }
 
 }
